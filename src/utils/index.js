c889809d143efdf5a3fe242832b8764868ecb597
julcsi <ejulcsi@gmail.com>
2019-08-21 21:57:36 +0200

    finetune

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,12 @@
+const OWNER = 'cégtulajdonos';
+const MANAGER = 'cégvezető';
+
 export const getOligarchData = (data) => {
 	if (!data.oligarch || !data.ceo) {
 		return
 	}
-	const oligarchs = data.oligarch.filter(ol => ol !== '').map(i => ({type: 'cégvezető', name: i}))
-	const ceos = data.ceo.filter(ol => ol !== '').map(i => ({type: 'üzletvezető', name: i}))
+	const oligarchs = data.oligarch.filter(ol => ol !== '').map(i => ({type: OWNER, name: i}))
+	const ceos = data.ceo.filter(ol => ol !== '').map(i => ({type: MANAGER, name: i}))
 
 	const allOligarchs = [...oligarchs, ...ceos]
 	const oligarchMap = allOligarchs.reduce((a, c) => {
