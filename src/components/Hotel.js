120c081dc6e95b7a1b7ae1405c97cd5e43936f70
juli_e <julia.ero@euedge.com>
2019-09-10 14:24:31 +0200

    names with link

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -19,80 +19,86 @@ import orangeIcon from '../assets/marker-icon-orange.svg';
 import { getOligarchData } from '../utils';
 
 const Hotel = (props) => {
-  const hotelById = places.find(item => item.properties.id === parseInt(props.id));
-  const data = hotelById.properties;
-  const [lat, lng] = hotelById.geometry.coordinates;
+    const hotelById = places.find(item => item.properties.id === parseInt(props.id));
+    const data = hotelById.properties;
+    const [lat, lng] = hotelById.geometry.coordinates;
 
-  const icon = L.icon({
-    iconUrl: orangeIcon,
-    shadowUrl: iconShadow,
-    iconSize: [40, 62],
-    iconAnchor: [20, 52],
-    shadowSize: [40, 62],
-    shadowAnchor: [12, 62]
-  });
+    const icon = L.icon({
+        iconUrl: orangeIcon,
+        shadowUrl: iconShadow,
+        iconSize: [40, 62],
+        iconAnchor: [20, 52],
+        shadowSize: [40, 62],
+        shadowAnchor: [12, 62],
+    });
 
-  const goBack = () => {
-    // store.dispatch(closePopup())
-    props.history.push('/');
-    store.dispatch(setSelectedPoint(hotelById));
-    setTimeout(() => store.dispatch(setCenter([lat, lng])), 100);
-  };
+    const goBack = () => {
+        // store.dispatch(closePopup())
+        props.history.push('/');
+        store.dispatch(setSelectedPoint(hotelById));
+        setTimeout(() => store.dispatch(setCenter([lat, lng])), 100);
+    };
 
-  const oligarchData = getOligarchData(data.oligarchs, data.ceos);
+    const oligarchData = getOligarchData(data.oligarchs, data.ceos);
 
-  return (
-    <div className={[styles.hotel, 'hotel'].join(' ')}>
-      <div className={styles.hotelWrapper}>
-        <div className={styles.info}>
-          <h1>{data.name}</h1>
-          <div className={styles.hotelRow}>
-            <p>Hely típusa: <span>{data.type}</span></p>
-          </div>
-          <div className={styles.hotelRow}>
-            <Icon img={hotelIcon} size="small"/>
-            <p>Üzemeltető: <span>{data.company}</span></p>
-          </div>
-          <div className={styles.hotelRow}>
-            <Icon img={horseIcon} size="small"/>
-            <p>Kapcsolódó személyek:<br/>
-              {oligarchData.map(oligarch => (<span>{oligarch.name}<span className={styles.title}> ({oligarch.data.type})</span><br/></span>))}
-            </p>
-          </div>
-          <div className={styles.hotelRow}>
-            <Icon img={pinIcon} size="small"/>
-            <p>Cím: <span>{data.address}</span></p>
-          </div>
-          {data.link !== '' && (<div className={styles.hotelRow}>
-            <Icon img={linkIcon} size="small"/>
-            <a href={data.link} target="_blank"><span>Kapcsolódó cikk</span></a>
-          </div>)}
-          {data.details !== '' && (
-            <div className={styles.hotelRow}>
-              <p>A hely azért szerepel, mert:<br/>{data.details}</p>
+    return (
+        <div className={[styles.hotel, 'hotel'].join(' ')}>
+            <div className={styles.hotelWrapper}>
+                <div className={styles.info}>
+                    <h1>{data.name}</h1>
+                    <div className={styles.hotelRow}>
+                        <p>Hely típusa: <span>{data.type}</span></p>
+                    </div>
+                    <div className={styles.hotelRow}>
+                        <Icon img={hotelIcon} size="small"/>
+                        <p>Üzemeltető: <span>{data.company}</span></p>
+                    </div>
+                    <div className={styles.hotelRow}>
+                        <Icon img={horseIcon} size="small"/>
+                        <p>Kapcsolódó személyek:<br/>
+                            {oligarchData.map(oligarch => (
+                                <span className={styles.oligarch}>
+                                    <a href={oligarch.data.link} target="_blank">{oligarch.name}</a>
+                                    <span className={styles.title}> ({oligarch.data.type})</span><br/>
+                                </span>
+                            ))}
+                        </p>
+                    </div>
+                    <div className={styles.hotelRow}>
+                        <Icon img={pinIcon} size="small"/>
+                        <p>Cím: <span>{data.address}</span></p>
+                    </div>
+                    {data.link !== '' && (<div className={styles.hotelRow}>
+                        <Icon img={linkIcon} size="small"/>
+                        <a href={data.link} target="_blank"><span>Kapcsolódó cikk</span></a>
+                    </div>)}
+                    {data.details !== '' && (
+                        <div className={styles.hotelRow}>
+                            <p>A hely azért szerepel, mert:<br/>{data.details}</p>
+                        </div>
+                    )}
+                    {data.date !== '' && (
+                        <div className={styles.hotelRow}>
+                            <p>Adatok frissítve: <span>{data.date}</span></p>
+                        </div>
+                    )}
+                    <div className={styles.back} onClick={() => goBack()}>
+                        <Icon img={arrowIcon} size="large"/>
+                    </div>
+                </div>
+                <div className={styles.map}>
+                    <LeafletMap className="markercluster-map" center={[lat, lng]} zoom={17} maxZoom={19}
+                                zoomControl={false}>
+                        <TileLayer
+                            url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
+                            attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/attributions'>CARTO</a>"
+                        />
+                        <Marker position={[lat, lng]} icon={icon}/>
+                    </LeafletMap>
+                </div>
             </div>
-          )}
-          {data.date !== '' && (
-            <div className={styles.hotelRow}>
-              <p>Adatok frissítve: <span>{data.date}</span></p>
-            </div>
-          )}
-          <div className={styles.back} onClick={() => goBack()}>
-            <Icon img={arrowIcon} size="large"/>
-          </div>
         </div>
-        <div className={styles.map}>
-          <LeafletMap className="markercluster-map" center={[lat, lng]} zoom={17} maxZoom={19} zoomControl={false}>
-            <TileLayer
-              url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
-              attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/attributions'>CARTO</a>"
-            />
-            <Marker position={[lat, lng]} icon={icon}/>
-          </LeafletMap>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Hotel;
