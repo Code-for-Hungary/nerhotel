e2afa3dd86a5f4ffc8ffb42eb0970199e68f1d49
julcsi <ejulcsi@gmail.com>
2020-06-19 23:50:14 +0200

    hotel page fix

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -60,7 +60,7 @@ const Hotel = (props) => {
               </p>
             </div>
           )}
-          {data.oligarchData && data.oligarchData.length > 0 && (
+          {oligarchData && oligarchData.length > 0 && (
             <div className={styles.hotelRow}>
               <Icon img={horseIcon} size="small"/>
               <p>Kapcsolódó személyek:<br/>
