fb54de0d1b52a268894992810e1fff1f7bb0d8fb
David Veszelovszki <veszelovszki@gmail.com>
2020-10-07 15:58:56 +0200

    Added data export page with a simple content, and a link to it in the menu.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HotelView from './views/HotelView';
 import MapView from './views/MapView';
 import AboutView from './views/AboutView';
 import ContactView from './views/ContactView';
+import DataExportView from './views/DataExportView';
 
 import { MapContext, HotelContext } from './context';
 import reducer, { initialState } from './reducer';
@@ -27,6 +28,7 @@ function App () {
               <Route path="/hotel/:id" exact component={HotelView}/>
               <Route path="/about" exact component={AboutView}/>
               <Route path="/contact" exact component={ContactView}/>
+              <Route path="/data-export" exact component={DataExportView}/>
             </Switch>
           </HashRouter>
         </MapContext.Provider>
