2bf86d09e02edc564215b4b8350ff904be1a44ad
David Veszelovszki <veszelovszki@gmail.com>
2020-10-27 22:00:56 +0100

    Marked main oligarchs with a horse icon.

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,7 +3,7 @@ import styles from '../css/icon.module.css';
 
 export default function (props) {
   return (
-    <div className={[styles.iconWrapper, styles[props.size]].join(' ')}>
+    <div className={[styles.iconWrapper, styles[props.size]].join(' ') + ' ' + (props.className || '')}>
       <img src={props.img} alt=""/>
     </div>
   );
