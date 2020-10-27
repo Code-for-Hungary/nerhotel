582e1b04ed5cbf6f612ac692ff2118f4e9986c9d
David Veszelovszki <veszelovszki@gmail.com>
2020-10-27 21:33:33 +0100

    Moved the CSV URL to a common config to avoid duplicate hard-coding this data.

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+  csvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSEboU5aIOUgZ-hmNpLQIYB8EZTc1HYAFf9mL97jvjVl6S9auEiFxJ1fwMpbr6-7dwPYl57BOK4ANfs/pub?gid=0&single=true&output=csv'
+};
