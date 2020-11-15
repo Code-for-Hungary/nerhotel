c454c306c272be02dc4d4d08b1e3e21dae089ad5
ejulcsi <ejulcsi@gmail.com>
2020-11-15 20:54:26 +0100

    revert csv parse + tiby refactor

--- src/utils/csvParser.js
+++ src/utils/csvParser.js
@@ -4,27 +4,50 @@
  * @returns {Object<string, string>[]} Array of objects where the keys are the headers, values the cell values.
  */
 export function convertCsvToObject(csvString) {
-  let { header, rows} = parseCSV(csvString);
+  const [headers, ...rows] = csvToArray(csvString);
+
   return rows.map(row => row
     .reduce((result, value, index) => {
-      result[header[index]] = value;
+      result[headers[index]] = value;
       return result;
     }, {}));
 }
 
 /**
+ * RFC 4180 compatible CSV parser.
  * @param {string} csvString Must be comma-separated, optionally quoted with double quotes, double quotes escaped
  *        with another double quote (Excel style).
- * @returns {Object<string[], string[][]>} An object with the header fields and the array of rows with their fields
+ * @returns {[[string]]} The CSV content as a 2D array of strings
  */
-function parseCSV(string) {
-  const [header, ...rows] = string.split('\n');
-  const headerWithFields = header.split(',').map(i => i.replace(/"/g, ''));
-  const rowsWithFields = rows.map(row => {
-    const fields = row.split(',"').map(i => i.replace(/"/g, ''));
-    return fields;
-  })
-
-  return { header: headerWithFields, rows: rowsWithFields };
+export function csvToArray(csvString) {
+  let currentCharacter;
+  let previousCharacter = '';
+  let rowIndex = 0;
+  let row = [''];
+  let fieldIndex = 0;
+  let areWeInsideField = false;
+  let returnValue = [row];
+  for (currentCharacter of csvString) {
+    if (currentCharacter === '"') {
+      if (previousCharacter === '"' && !areWeInsideField) {
+        row[fieldIndex] += '"';
+      }
+      areWeInsideField = !areWeInsideField;
+    } else if (currentCharacter === ',' && !areWeInsideField) {
+      fieldIndex++;
+      row[fieldIndex] = '';
+    } else if ((currentCharacter === '\n') && !areWeInsideField) {
+      if (previousCharacter === '\r') {
+        row[fieldIndex] = row[fieldIndex].slice(0, -1);
+      }
+      rowIndex++;
+      row = [''];
+      returnValue[rowIndex] = row;
+      fieldIndex = 0;
+    } else {
+      row[fieldIndex] += currentCharacter;
+    }
+    previousCharacter = currentCharacter;
+  }
+  return returnValue;
 }
-
