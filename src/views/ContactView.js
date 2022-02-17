7ff4463a1a567ae3e9d7b02ef22260b7dfa35d20
attila juhász <juhasz.attila@k-monitor.hu>
2022-02-17 09:12:51 +0000

    támogass

--- src/views/ContactView.js
+++ src/views/ContactView.js
@@ -15,12 +15,13 @@ const AboutView = (props) => {
           <p>A K-Monitor Iroda 2007 óta küzd a közpénzek átlátható felhasználásáért és a korrupció visszaszorításáért.
             Civil szervezetünk olyan politikai, gazdasági és társadalmi környezetet megteremtéséért dolgozik, ahol
             az állampolgárok elutasítják, a hatóságok pedig hatékonyan feltárják és szankcionálják a korrupciót.</p>
+            <p><a href="https://tamogatas.k-monitor.hu/"> target="new">Támogasd munkánkat rendszeres adományoddal!</a></p>
         </section>
         <section>
           <h2>Elérhetőség</h2>
           <p><strong>K-Monitor Közhasznú Egyesület</strong></p>
           <p>Levelezési cím: 1062 Budapest, Bajza u. 23.</p>
-          <a href="mailto:info@k-monitor.hu">info@k-monitor.hu</a> 
+          <a href="mailto:info@k-monitor.hu">info@k-monitor.hu</a>&nbsp;
           <a href="https://k-monitor.hu" target="new">k-monitor.hu</a>
 
         </section>
