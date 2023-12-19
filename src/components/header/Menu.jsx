835dd2df6fc554a1a856e8e82c57ad3ab36c31f5
Tamas Kuti <cooty13@gmail.com>
2023-12-19 15:51:17 +0100

    add backdrop to menu

--- src/components/header/Menu.jsx
+++ src/components/header/Menu.jsx
@@ -14,6 +14,7 @@ import "./Menu.transition.css";
 
 const Menu = () => {
     const menuRef = useRef(null);
+    const backdropRef = useRef(null);
     const { dispatch, showMenu } = useContext(MapContext);
     const { t, i18n } = useTranslation();
     const { resolvedLanguage } = i18n;
@@ -22,72 +23,77 @@ const Menu = () => {
     }, [dispatch]);
 
     return (
-        <CSSTransition in={showMenu} nodeRef={menuRef} classNames="Menu" unmountOnExit timeout={200}>
-            <nav className={styles.menu} ref={menuRef}>
-                <div className={styles.content}>
-                    <div className={styles.close}>
-                        <button onClick={closeMenu} type="button" className="resetButton">
-                            <Icon img={closeIcon} size="large" />
-                        </button>
+        <>
+            <CSSTransition in={showMenu} nodeRef={menuRef} classNames="Menu" unmountOnExit timeout={200}>
+                <nav className={styles.menu} ref={menuRef}>
+                    <div className={styles.content}>
+                        <div className={styles.close}>
+                            <button onClick={closeMenu} type="button" className="resetButton">
+                                <Icon img={closeIcon} size="large" />
+                            </button>
+                        </div>
+
+                        <div className={styles.logoWrapper}>
+                            <img src={resolvedLanguage === "hu" ? logo : logoEn} alt="" style={{ aspectRatio: "47 / 112" }} />
+                        </div>
+
+                        <ul className={styles.menulist}>
+                            <li>
+                                <SmartLink to="/" onClick={closeMenu}>
+                                    {t("navigation.map")}
+                                </SmartLink>
+                            </li>
+                            <li>
+                                <SmartLink to="/about" onClick={closeMenu}>
+                                    {t("navigation.about")}
+                                </SmartLink>
+                            </li>
+                            <li>
+                                <SmartLink to={t("navigation.submit-link")} onClick={closeMenu}>
+                                    {t("navigation.submit")}
+                                </SmartLink>
+                            </li>
+                            <li>
+                                <SmartLink to="/contact" onClick={closeMenu}>
+                                    {t("navigation.contact")}
+                                </SmartLink>
+                            </li>
+                            <li>
+                                <SmartLink to="https://tamogatas.k-monitor.hu" onClick={closeMenu}>
+                                    {t("navigation.supportUs")}
+                                </SmartLink>
+                            </li>
+                            <li>
+                                <SmartLink to="/data-export" onClick={closeMenu}>
+                                    {t("navigation.export")}
+                                </SmartLink>
+                            </li>
+                        </ul>
+
+                        <div className={styles.imageWrapper}>
+                            <img src={image} alt="" />
+                        </div>
+
+                        <address className={styles.footer}>
+                            <p>
+                                <strong
+                                    dangerouslySetInnerHTML={{
+                                        __html: t("navigation.address.name"),
+                                    }}
+                                />
+                            </p>
+                            <p>{t("navigation.address.heading")}:</p>
+                            <p>{t("navigation.address.address")}</p>
+                            <SmartLink to="info@k-monitor.hu">info@k-monitor.hu</SmartLink>
+                        </address>
                     </div>
-
-                    <div className={styles.logoWrapper}>
-                        <img src={resolvedLanguage === "hu" ? logo : logoEn} alt="" style={{ aspectRatio: "47 / 112" }} />
-                    </div>
-
-                    <ul className={styles.menulist}>
-                        <li>
-                            <SmartLink to="/" onClick={closeMenu}>
-                                {t("navigation.map")}
-                            </SmartLink>
-                        </li>
-                        <li>
-                            <SmartLink to="/about" onClick={closeMenu}>
-                                {t("navigation.about")}
-                            </SmartLink>
-                        </li>
-                        <li>
-                            <SmartLink to={t("navigation.submit-link")} onClick={closeMenu}>
-                                {t("navigation.submit")}
-                            </SmartLink>
-                        </li>
-                        <li>
-                            <SmartLink to="/contact" onClick={closeMenu}>
-                                {t("navigation.contact")}
-                            </SmartLink>
-                        </li>
-                        <li>
-                            <SmartLink to="https://tamogatas.k-monitor.hu" onClick={closeMenu}>
-                                {t("navigation.supportUs")}
-                            </SmartLink>
-                        </li>
-                        <li>
-                            <SmartLink to="/data-export" onClick={closeMenu}>
-                                {t("navigation.export")}
-                            </SmartLink>
-                        </li>
-                    </ul>
-
-                    <div className={styles.imageWrapper}>
-                        <img src={image} alt="" />
-                    </div>
-
-                    <address className={styles.footer}>
-                        <p>
-                            <strong
-                                dangerouslySetInnerHTML={{
-                                    __html: t("navigation.address.name"),
-                                }}
-                            />
-                        </p>
-                        <p>{t("navigation.address.heading")}:</p>
-                        <p>{t("navigation.address.address")}</p>
-                        <SmartLink to="info@k-monitor.hu">info@k-monitor.hu</SmartLink>
-                    </address>
-                </div>
-                <PoweredByVercel link={"https://vercel.com/"} />
-            </nav>
-        </CSSTransition>
+                    <PoweredByVercel link={"https://vercel.com/"} />
+                </nav>
+            </CSSTransition>
+            <CSSTransition in={showMenu} nodeRef={backdropRef} classNames="MenuBackdrop" unmountOnExit timeout={200}>
+                <div className={styles.backdrop} ref={backdropRef} onClick={closeMenu} />
+            </CSSTransition>
+        </>
     );
 };
 
