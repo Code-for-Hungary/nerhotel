cb617164fa360620cad42a27bb99573210f4060a
boa <boa@protonmail.com>
2026-01-06 14:18:29 +0100

    change share icon

--- src/components/ShareLinkControl.jsx
+++ src/components/ShareLinkControl.jsx
@@ -1,12 +1,12 @@
 import styles from "./ShareLinkControl.module.css";
 import listStyles from "../css/map-list-opener.module.css";
 import Icon from "./ui/Icon";
-import linkIcon from "../assets/link-icon.svg";
+import shareIcon from "../assets/share-icon.svg";
 
 function ShareLinkControl({ shareLink }) {
     return (
         <button className={`${listStyles.controlButton} ${styles.shareButton}`} onClick={shareLink}>
-            <Icon img={linkIcon} />
+            <Icon img={shareIcon} />
         </button>
     );
 }
