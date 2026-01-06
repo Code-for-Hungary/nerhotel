0778a6afe548ab6168530db4fea62f46e4d5548f
boa <boa@protonmail.com>
2026-01-06 14:08:21 +0100

    clean up share button styles

--- src/components/ShareLinkControl.jsx
+++ src/components/ShareLinkControl.jsx
@@ -1,10 +1,11 @@
 import styles from "./ShareLinkControl.module.css";
+import listStyles from "../css/map-list-opener.module.css";
 import Icon from "./ui/Icon";
 import linkIcon from "../assets/link-icon.svg";
 
 function ShareLinkControl({ shareLink }) {
     return (
-        <button className={`${styles.controlButton} ${styles.shareButton}`} onClick={shareLink}>
+        <button className={`${listStyles.controlButton} ${styles.shareButton}`} onClick={shareLink}>
             <Icon img={linkIcon} />
         </button>
     );
